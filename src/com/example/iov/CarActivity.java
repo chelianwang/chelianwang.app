33f0b33b1d52c04caf4d3a3428b43e4b28f97a71
chelianwang <1665792276@qq.com>
2016-09-12 08:27:05 +0800

    Add files via upload

---
+++ src/com/example/iov/CarActivity.java
@@ -0,0 +1,15 @@
+package com.example.iov;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.main_car);
+    	
+    	
+    	
+    }
+}
---
+++ src/com/example/iov/MoreActivity.java
@@ -0,0 +1,12 @@
+package com.example.iov;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoreActivity  extends Activity{
+   @Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main_more);
+}
+}
